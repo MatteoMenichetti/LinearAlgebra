c5133e688af27b27a09f01d60893ec29ad2a0d90
matteo <matteo.menichetti@stud.unifi.it>
2021-12-08 12:45:37 +0100

    to be continued

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
-#include <math.h>
 #include <time.h>
 #include <stdlib.h>
 
 #define RANDOM rand() % (10)
 #define INITIALIZE srand(time(NULL))
 
-#define DIML 3
-#define DIMC DIML
+#define DIMR 3
+#define DIMC DIMR
+
+/* 1    2   3       9   8   7   6
+ * 4    5   6       5   4   3   2
+ * 7    8   9       1   0   -1  -2
+ */
+
+void mul(int rowA, int colA, int a[rowA][colA], int rowB, int colB, int b[rowB][colB]) {
+    if(colA!=rowB)return;
+    //???
+}
 
 void createMatrix(int row, int col, int m[row][col]) {
     for (int i = 0; i < row; i++) {
@@ -21,9 +30,11 @@ void createMatrix(int row, int col, int m[row][col]) {
 
 int main() {
     INITIALIZE;
-    int col = 6, a[DIML][DIMC], b[DIMC][col];
-    createMatrix(DIML, DIMC, a);
+    int col = RANDOM, a[DIMR][DIMC], b[DIMC][col];
+    createMatrix(DIMR, DIMC, a);
     createMatrix(DIMC, col, b);
+
+    mul(DIMR, DIMC, a, DIMC, col, b);
 
     return 0;
 }
