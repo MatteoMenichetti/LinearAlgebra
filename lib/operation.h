4fe859f1700458f0b10a75fde7ad922f0c94b50b
matteo <matteo.menichetti@stud.unifi.it>
2021-12-23 18:52:54 +0100

    add ; to correct the compilation

--- lib/operation.h
+++ lib/operation.h
@@ -1,3 +1,3 @@
 #include <stdio.h>
 
-void mul(int rowA, int colA, int a[rowA][colA], int rowB, int colB, int b[rowB][colB])
+void mul(int rowA, int colA, int a[rowA][colA], int rowB, int colB, int b[rowB][colB], int AB[rowA][colB]);
