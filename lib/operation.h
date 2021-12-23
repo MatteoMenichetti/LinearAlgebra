5bd74f7460b46bab261055a98bc59039a9797ecb
matteo <matteo.menichetti@stud.unifi.it>
2021-12-23 19:06:45 +0100

    add sum (not tested)

--- lib/operation.h
+++ lib/operation.h
@@ -1,3 +1,5 @@
 #include <stdio.h>
 
 void multiplication(int rowA, int colA, int a[rowA][colA], int rowB, int colB, int b[rowB][colB], int AB[rowA][colB]);
+
+void sum(int row, int col, int ApB[row][col]);
