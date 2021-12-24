83321b77e9749a5b7324059f4587b946e7b4c83b
matteo <matteo.menichetti@stud.unifi.it>
2021-12-24 17:55:34 +0100

    add parameters (the matrices) for the sum

--- lib/operation.h
+++ lib/operation.h
@@ -2,4 +2,4 @@
 
 void multiplication(int rowA, int colA, int a[rowA][colA], int rowB, int colB, int b[rowB][colB], int AB[rowA][colB]);
 
-void sum(int row, int col, int ApB[row][col]);
+void sum(int row, int col, int A[row][col],int B[row][col], int ApB[row][col]);
