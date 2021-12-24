dd8894590c79dc9e08a49650850d2fcd8ac4c1b5
matteo <matteo.menichetti@stud.unifi.it>
2021-12-24 17:54:09 +0100

    rename sum in multiplication_sum for use this name in sum.c

--- src/OperationBetweenMatrix/product.c
+++ src/OperationBetweenMatrix/product.c
@@ -1,4 +1,4 @@
-int sum(const int *a, int rowB, int colB, int j, int b[rowB][colB]) {
+int multiplication_sum(const int *a, int rowB, int colB, int j, int b[rowB][colB]) {
     int c = 0, limiter = rowB;
     for (int i = 0; i < limiter; i++)
         c += a[i] * b[i][j];
@@ -10,7 +10,7 @@ void multiplication(int rowA, int colA, int a[rowA][colA], int rowB, int colB, i
     int colAB = 0, rowAB = 0;
     for (int i = 0; i < rowA; i++) {
         for (int j = 0; j < colB; j++)
-            AB[rowAB][colAB++] = sum(&a[i][0], rowB, colB, j, b);
+            AB[rowAB][colAB++] = multiplication_sum(&a[i][0], rowB, colB, j, b);
         rowAB++;
         colAB = 0;
     }
