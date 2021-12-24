51f8598ec69f0e3c15f1084e2201aa742882310f
matteo <matteo.menichetti@stud.unifi.it>
2021-12-24 17:53:21 +0100

    rename matrix use for the sum

--- src/OperationBetweenMatrix/sum.c
+++ src/OperationBetweenMatrix/sum.c
@@ -1,3 +1,3 @@
 void sum(int row, int col, int A[row][col], int B[row][col], int ApB[row][col]) {
-    for (int r = 0; r < row; r++)for (int c = 0; c < col; c++)ApB[r][c] = A[r][c] + b[r][c];
+    for (int r = 0; r < row; r++)for (int c = 0; c < col; c++)ApB[r][c] = A[r][c] + B[r][c];
 }
