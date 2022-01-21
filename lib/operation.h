8006fbb55715b07ef3748a6b7ea9028a21c3b168
matteo <matteo.menichetti@stud.unifi.it>
2022-01-21 19:41:55 +0100

    add prototype on creation of matrix

--- lib/operation.h
+++ lib/operation.h
@@ -3,3 +3,5 @@
 void multiplication(int rowA, int colA, int a[rowA][colA], int rowB, int colB, int b[rowB][colB], int AB[rowA][colB]);
 
 void sum(int row, int col, int A[row][col],int B[row][col], int ApB[row][col]);
+
+int ** createMatrix (int row, int column);
