94228aab77a73f4396db8f9c102f3886fb12081f
matteo <matteo.menichetti@stud.unifi.it>
2022-01-23 14:34:13 +0100

    first implementation of createMatrix. The firsts two positions store the specs of matrix (row and columns); each row is addressed by pointer.

--- src/createMatrix.c
+++ src/createMatrix.c
@@ -1,5 +1,11 @@
 #include "../lib/operation.h"
 
-int ** createMatrix(int row, int column){
-    return 0;
+int **createMatrix(int row, int column) {
+    int **matrix = (int **) calloc(row + 2, sizeof(int));
+    *matrix[0] = row;
+    *matrix[1] = column;
+    for (int i = 2; i < row + 2; i++) {
+        matrix[i] = (int *) calloc(column + 1, sizeof(int));
+    }
+    return matrix;
 }
