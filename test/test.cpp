50cc23177f90db8469c7869cd95109c91118e935
matteo <matteo.menichetti@stud.unifi.it>
2022-01-30 17:16:58 +0100

    add test on creation of matrix

--- test/test.cpp
+++ test/test.cpp
@@ -1,2 +1,8 @@
 #include "gtest/gtest.h"
 #include "../lib/operation.h"
+
+TEST(test, creationOfMatrix) {
+    int row = 2, column = 3, **matrix = createMatrix(row, column);
+    ASSERT_EQ(row, *matrix[0]);
+    ASSERT_EQ(column, *matrix[1]);
+}
