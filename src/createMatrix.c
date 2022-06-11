96c2923b8a9ca74dd7ba6960c040dad5898bc814
menni <mattei.menichetti@stud.unifi.it>
2022-06-11 12:01:13 +0200

    add the true use of pointer for dimension of matrix

--- src/createMatrix.c
+++ src/createMatrix.c
@@ -2,8 +2,8 @@
 
 int **createMatrix(int row, int column) {
     int **matrix = (int **) calloc(row + 2, sizeof(int));
-    *matrix[0] = row;
-    *matrix[1] = column;
+    matrix[0] = &row;
+    matrix[1] = &column;
     for (int i = 2; i < row + 2; i++) {
         matrix[i] = (int *) calloc(column + 1, sizeof(int));
     }
