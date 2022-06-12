f2f324d2baa2c2aab5b5d0f6d5a8f552cfa0aee8
menni <mattei.menichetti@stud.unifi.it>
2022-06-12 11:15:52 +0200

    refactoring equality test

--- test/CreationMatrix.cpp
+++ test/CreationMatrix.cpp
@@ -2,14 +2,21 @@
 #include <gmock/gmock.h>
 #include "../src/createMatrix.c"
 
-TEST(creationMatrix, testMatrix1Row) {
-    int row = 1, column = 3, **matrix = createMatrix(row, column);
-    EXPECT_THAT(column, testing::Eq(*matrix[1]));
-    EXPECT_THAT(row, testing::Eq(*matrix[0]));
+void comparison(int **matrix, int column, int row) {
+    EXPECT_EQ(column, *matrix[1]);
+    EXPECT_EQ(row, *matrix[0]);
     free(matrix);
 }
 
-TEST(creationMatrix, testMatrix1Col){
-    int row=3, column = 1;
+TEST(creationMatrix, testMatrix1Row) {
+    int row = 1, column = 3, **matrix = createMatrix(row, column);
 
+    comparison(matrix, column, row);
+}
+
+
+TEST(creationMatrix, testMatrix1Col) {
+    int row = 3, column = 1, **matrix = createMatrix(row, column);
+
+    comparison(matrix, column, row);
 }
