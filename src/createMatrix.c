cf178e6e0273a6d466e991593fe69a4b865c1841
menni <mattei.menichetti@stud.unifi.it>
2022-06-12 11:31:59 +0200

    add control on row and column value: if they are 0 createMatrix return 0

--- src/createMatrix.c
+++ src/createMatrix.c
@@ -1,6 +1,9 @@
 #include "../lib/operation.h"
 
 int **createMatrix(int row, int column) {
+
+    if (row == 0 || column == 0)return 0;
+
     int **matrix = (int **) calloc(row + 2, sizeof(int));
     for (int i = 0; i < row + 2; i++) {
         matrix[i] = (int *) calloc(column + 1, sizeof(int));
