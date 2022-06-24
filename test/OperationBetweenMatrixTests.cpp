fe9d81153c98b9b47a3d22c9c7bf2c92ac9ad10a
menni <mattei.menichetti@stud.unifi.it>
2022-06-24 11:00:26 +0200

    introduction first test on matrix sum

--- test/OperationBetweenMatrixTests.cpp
+++ test/OperationBetweenMatrixTests.cpp
@@ -1,11 +1,23 @@
 #include <gtest/gtest.h>
 #include "../lib/operation.h"
-#include "./randomizeValueMatrix.c"
+#include "../lib/indexing.h"
+#include "../src/OperationBetweenMatrix/sum.c"
 
 TEST(OperationBetweenMatrixTests, sumBewtween1RowMatrix) {
-    int row = 1, column = 3, **matrixA = createMatrix(1, 3), **matrixB= createMatrix(row, column);
-    randomize(matrixA);
-    int i = matrixA[2][0];
-    i=matrixA[2][1];
+    int row = 1, column = 3, **matrixA = createMatrix(row, column), **matrixB = createMatrix(row,
+                                                                                             column), **matrixS;
+
+    for (int c = 0; c < column - 2; c++) {
+        for (int r = 0; r < row; r++) {
+            matrixA[COL(c)][ROW(r)] = 1;
+            matrixB[COL(c)][ROW(r)] = 2;
+        }
+    }
+
+    int i = matrixA[COL(1)][0];
     i++;
+    matrixS = sum(matrixA, matrixB);
+    for (int c = 0; c < column - 2; c++)
+        for (int r = 0; r < row; r++)
+            EXPECT_EQ(matrixA[COL(c)][ROW(r)] + matrixB[COL(c)][ROW(r)], matrixS[COL(c)][ROW(r)]);
 }
