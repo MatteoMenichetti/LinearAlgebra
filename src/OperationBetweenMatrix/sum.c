b532b2d1b6c15b5a0a0e041295d152aeeb4b7a84
menni <mattei.menichetti@stud.unifi.it>
2022-07-01 12:43:38 +0200

    rename sum1 in sumMatrix and update the index to correct index using macros

--- src/OperationBetweenMatrix/sum.c
+++ src/OperationBetweenMatrix/sum.c
@@ -1,15 +1,18 @@
 #include "../../lib/operation.h"
+#include "../../lib/indexing.h"
 
-void sum1(int row, int col, int **A, int **B, int **ApB) {
-    for (int r = 2; r < row+2; r++)for (int c = 0; c < col; c++)ApB[r][c] = A[r][c] + B[r][c];
+void sumMatrix(int **A, int **B, int **ApB) {
+    for (int r = 0; r < *A[ROWP]; r++)
+        for (int c = 0; c < *A[COLP]; c++)
+            ApB[ROW(r)][COL(c)] = A[ROW(r)][COL(c)] + B[ROW(r)][COL(c)];
 }
 
 int **sum(int **a, int **b) {
-    if (*a[0] != *b[0] || *a[1] != *b[1]) {
+    if (*a[ROWP] != *b[ROWP] || *a[COLP] != *b[COLP]) {
         perror("matrix with != numbers of row || column");
         exit(EXIT_FAILURE);
     }
-    int **ApB = createMatrix(*a[0],*a[1]);
-    sum1(*a[0], *a[1], a, b, ApB);
+    int **ApB = createMatrix(*a[0], *a[1]);
+    sumMatrix(a, b, ApB);
     return ApB;
 }
