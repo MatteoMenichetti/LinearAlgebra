b6bdbd802268059d4885f25c488c53c2aa32ef0a
menni <mattei.menichetti@stud.unifi.it>
2022-07-10 19:18:51 +0200

    .

--- src/OperationBetweenMatrix/sum.c
+++ src/OperationBetweenMatrix/sum.c
@@ -2,9 +2,10 @@
 #include "../../lib/indexing.h"
 
 void sumMatrix(int **A, int **B, int **ApB) {
-    for (int r = 0; r < *A[ROWP]; r++)
-        for (int c = 0; c < *A[COLP]; c++)
+    for (int r = 0; r < A[0][ROWP]; r++)
+        for (int c = 0; c < A[0][COLP]; c++) {
             ApB[ROW(r)][COL(c)] = A[ROW(r)][COL(c)] + B[ROW(r)][COL(c)];
+        }
 }
 
 int **sum(int **a, int **b) {
@@ -12,7 +13,7 @@ int **sum(int **a, int **b) {
         perror("matrix with != numbers of row || column");
         exit(EXIT_FAILURE);
     }
-    int **ApB = createMatrix(*a[0], *a[1]);
+    int **ApB = createMatrix(a[0][ROWP], a[0][COLP]);
     sumMatrix(a, b, ApB);
     return ApB;
 }
