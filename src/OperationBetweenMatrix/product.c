a9d438a655017038d48d1fbba8ade003bcfc4af1
matteo <matteo.menichetti@stud.unifi.it>
2022-07-17 15:16:49 +0200

    pre-implementation of multiplication between matrices

--- src/OperationBetweenMatrix/product.c
+++ src/OperationBetweenMatrix/product.c
@@ -1,15 +1,21 @@
 #include "../../lib/operation.h"
 #include "../../lib/indexing.h"
 
-int multiplication_sum() {
-
-    return 0;
+int multiplication_sum(int r, int **matrixA, int **matrixB, int **matrixP) {
+    for (int i = 0; i < matrixA[COLP][0];i++)
+        return 0;
 }
 
-int **multiplication(int **a, int **b) {
-    if (a[COLP][0] != b[ROWP][0])return 0;
-
-
+int **multiplication(int **matrixA, int **matrixB) {
+    if (matrixA[COLP][0] != matrixB[ROWP][0] || matrixA[ROWP][0] != matrixB[COLP][0])return 0;
+    if (matrixA[ROWP][0] == matrixB[COLP][0]) {
+        int **matrixSwap = matrixA;
+        matrixA = matrixB;
+        matrixB = matrixSwap;
+    }
+    int **matrixP = createMatrix(matrixA[COLP][0], matrixA[COLP][0]);
+    for (int r = 0; r < matrixA[COLP][0]; r++)
+        multiplication_sum(r, matrixA, matrixB, matrixP);
 
     return 0;
 }
